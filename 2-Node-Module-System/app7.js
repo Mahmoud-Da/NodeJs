e724e3e068128e25be6ad45f0010b7a1785c43ed
Mahmoud-Da <mdabbagh6@gmail.com>
2025-10-10 12:10:35 +0900

    7- Path Module - #3

---
+++ 2-Node-Module-System/app7.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const pathObj = path.parse(__filename);
+console.log(pathObj);
+
+// {
+//   root: '/',
+//   dir: '/Users/mahmouddabbbagh/moody/NodeJs/2-Node-Module-System',
+//   base: 'app7.js',
+//   ext: '.js',
+//   name: 'app7'
+// }
