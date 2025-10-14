edb5aeea67534fcca6cb5edeb3c664c9dae13aab
Mahmoud-Da <mdabbagh6@gmail.com>
2025-10-14 10:15:27 +0900

    8- OS Module - #3

---
+++ 2-Node-Module-System/app8.js
@@ -0,0 +1,10 @@
+const os = require("os");
+
+const totalMemory = os.totalmem();
+const freeMemory = os.freemem();
+
+console.log("Total Memory: " + totalMemory);
+console.log("Free Memory: " + freeMemory);
+
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
