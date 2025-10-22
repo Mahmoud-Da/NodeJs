959e0678ad107d272bb68211f4e47a36663ce8f5
Mahmoud-Da <mdabbagh6@gmail.com>
2025-10-22 15:46:48 +0900

    11- Async and Await - #7

---
+++ 6-Asynchronous-JavaScript/index11.js
@@ -0,0 +1,51 @@
+console.log("Before");
+
+const promise = getUser(1);
+promise.then((user) => console.log(user));
+
+// getUser(1)
+//   .then((user) => getRepositories(user.gitHubUsername))
+//   .then((repos) => getCommits(repos[0]))
+//   .then((commits) => console.log(commits))
+//   .catch((err) => console.log("Error:", err.message));
+
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+}
+
+displayCommits();
+
+console.log("After");
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({ id: id, gitHubUsername: "mosh" });
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      reject(new Error("Could not get the repos"));
+    }, 2000);
+  });
+}
+
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling GitHub API...");
+      resolve(["commit"]);
+    }, 2000);
+  });
+}
