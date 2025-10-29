8fb0f6c977f432474283a6f27a8f98afdac08afd
Mahmoud-Da <mdabbagh6@gmail.com>
2025-10-29 12:01:38 +0900

    5- Validation Errors - #9

---
+++ 8-Mongo-Data-Validation/index5.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 20 },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+  },
+  author: String,
+  tags: {
+    type: Array,
+    validate: {
+      validator: async function (v) {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const result = v && v.length > 0;
+            resolve(result);
+          }, 4000);
+        });
+      },
+      message: "A course should have at least one tag.",
+    },
+  },
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+  },
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Node.js Course",
+    author: "Mahmoud",
+    category: "-",
+    tags: [],
+    isPublished: true,
+    price: 20,
+  });
+
+  try {
+    await course.save();
+  } catch (ex) {
+    for (field in err.errors) {
+      const validationError = err.errors[field];
+      console.log(validationError);
+    }
+  }
+}
+
+async function getCourses() {
+  const courses = await Course.find({ author: "Mahmoud", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .count();
+
+  console.log(courses);
+}
+
+async function updateCourse(id) {
+  const course = await Course.findByIdAndUpdate(id, {
+    $set: {
+      author: "Jack",
+      isPublished: true,
+    },
+  });
+
+  console.log(course);
+}
+
+async function removeCourse(id) {
+  const course = await Course.findByIdAndRemove(id);
+  console.log(course);
+}
+
+createCourse();
