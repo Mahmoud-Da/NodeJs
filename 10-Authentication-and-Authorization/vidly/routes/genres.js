2526c8129853e61041c93b3aabb71ae04075d54c
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-12 05:47:57 +0900

    18- Testing the Authorization - #11

--- 10-Authentication-and-Authorization/vidly/routes/genres.js
+++ 10-Authentication-and-Authorization/vidly/routes/genres.js
@@ -1,6 +1,7 @@
 const { Genre, validate } = require("../models/genre");
 const express = require("express");
 const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
