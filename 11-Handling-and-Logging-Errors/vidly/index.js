ab4d7863237c6bfdf13d9f258974334eced69bfa
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-13 10:25:28 +0900

    3- Express Error Middleware - #12

--- 11-Handling-and-Logging-Errors/vidly/index.js
+++ 11-Handling-and-Logging-Errors/vidly/index.js
@@ -8,6 +8,7 @@ const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require("./middleware/error");
 const express = require("express");
 const app = express();
 
@@ -24,5 +25,6 @@ app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 
+app.use(error);
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
