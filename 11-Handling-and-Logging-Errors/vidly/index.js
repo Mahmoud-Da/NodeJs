0b7f21227b693482c23c78b0a4ae43d466d1b19f
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-14 16:22:52 +0900

    13- Extracting the Logging Logic - #12

--- 11-Handling-and-Logging-Errors/vidly/index.js
+++ 11-Handling-and-Logging-Errors/vidly/index.js
@@ -1,8 +1,6 @@
-require("express-async-errors");
 require("dotenv").config();
-require("winston-mongodb");
 
-const winston = require("winston");
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
@@ -10,29 +8,10 @@ const app = express();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-process.on("uncaughtException", (exception) => {
-  console.log("We got an uncaught exception");
-  winston.error(exception.message, exception);
-});
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  winston.error(ex.message, ex);
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
-});
-
-winston.add(new winston.transports.Console());
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost:27017/vidly", // make sure to include port 27017
-    level: "error", // log only errors
-    collection: "log", // optional: name of log collection
-  })
-);
+}
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
