07dd6ecb4c03765f60673e78b1bfd1edd68805c2
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-24 11:39:11 +0900

    9- Testing if Rental Processed - #15

--- 14-Test-Driven-Development/vidly/routes/returns.js
+++ 14-Test-Driven-Development/vidly/routes/returns.js
@@ -15,6 +15,9 @@ router.post("/", async (req, res) => {
 
   if (!rental) return res.status(404).send("Rental not found");
 
+  if (rental.dateReturned)
+    return res.status(400).send("Rental is already processed");
+
   res.status(401).send();
 });
 
