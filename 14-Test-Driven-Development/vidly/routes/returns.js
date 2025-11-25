594629ae606d485e2d2b31fac7fbf4cb31df9532
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-25 14:53:05 +0900

    15- Refactoring the Validation Logic

--- 14-Test-Driven-Development/vidly/routes/returns.js
+++ 14-Test-Driven-Development/vidly/routes/returns.js
@@ -1,16 +1,13 @@
 const moment = require("moment");
+const validate = require("../middleware/validate");
 const { Rental } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const Joi = require("joi");
 
-router.post("/", auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send("Customer ID not provided");
-
-  if (!req.body.movieId) return res.status(400).send("Movie ID not provided");
-
+router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.findOne({
     "customer._id": req.body.customerId,
     "movie._id": req.body.movieId,
@@ -36,5 +33,14 @@ router.post("/", auth, async (req, res) => {
 
   res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
+
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
