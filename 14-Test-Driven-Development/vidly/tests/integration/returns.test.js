acdd353de41f11f745277b198543367650db4a77
Mahmoud-Da <mdabbagh6@gmail.com>
2025-11-25 12:15:57 +0900

    11- Testing the ReturnDate - #15

--- 14-Test-Driven-Development/vidly/tests/integration/returns.test.js
+++ 14-Test-Driven-Development/vidly/tests/integration/returns.test.js
@@ -83,6 +83,14 @@ describe("/api/returns", () => {
   it("should return 200 if we have a valid request", async () => {
     const res = await exec();
 
-    expect(res.status).toBe(400);
+    expect(res.status).toBe(200);
+  });
+
+  it("should return returnDate if input is valid", async () => {
+    const res = await exec();
+
+    const rentalInDb = await Rental.findById(rental._id);
+    const diff = new Date() - rentalInDb.dateReturned;
+    expect(diff).toBeLessThan(10 * 1000);
   });
 });
